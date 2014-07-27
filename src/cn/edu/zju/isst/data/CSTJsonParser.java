d55cee79df98d83421af8a645f3049ee6e9cafe9
suxingdeyi <tan0010022008@126.com>
2014-07-27 19:38:25 +0800

    增加了对复杂的集合类型和图的解析方法，和对嵌套型集合类与图的解析方法

--- src/cn/edu/zju/isst/data/CSTJsonParser.java
+++ src/cn/edu/zju/isst/data/CSTJsonParser.java
@@ -1,6 +1,9 @@
 package cn.edu.zju.isst.data;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule;
 import com.fasterxml.jackson.databind.JavaType;
 import org.json.JSONObject;
@@ -14,9 +17,11 @@ import java.util.Map;
 public class CSTJsonParser {
 
     private final static ObjectMapper mapper = new ObjectMapper();
+    private static TypeFactory typeFactory;
 
     static {
         mapper.registerModule(new JsonOrgModule());
+        typeFactory = mapper.getTypeFactory();
     }
 
     public static <T> T readByte(byte[] data, Class<T> clazz) throws IOException {
@@ -27,21 +32,91 @@ public class CSTJsonParser {
         return mapper.readValue(jsonString, clazz);
     }
 
+    public static Object readString(String jsonStr, JavaType javaType)throws JsonParseException,
+            JsonMappingException, IOException{
+        Object result = mapper.readValue(jsonStr, javaType);
+        return result;
+    }
+
+    /**
+     * 用于解析简单的集合类型
+     * */
+    public static <T> List<T> readString(String jsonStr, Class <? extends List> collectionClazz,
+                                         Class <T> elementClazz) throws JsonParseException, JsonMappingException, IOException{
+        JavaType javaType = collectionToJavaType(collectionClazz, elementClazz);
+        List <T> listResult  = mapper.readValue(jsonStr, javaType);
+        return listResult;
+    }
+    /*
+     * 用于解析复合的集合类型
+     * */
+    public static <T> List<T> readString(String jsonObj, Class <? extends List> collectionClazz,
+                                         JavaType javaTypeElement) throws JsonParseException, JsonMappingException, IOException{
+        JavaType javaType = collectionToJavaType(collectionClazz, javaTypeElement);
+        List <T> listResult = mapper.readValue(jsonObj, javaType);
+        return listResult;
+    }
+
+
+    public static <K, V> Map<K, V> readString(String jsonStr, Class <? extends Map> mapClazz ,Class<K> keyClazz,
+                                              Class<V> valueClazz) throws JsonParseException, JsonMappingException, IOException{
+        JavaType javaType = mapToJavaType(mapClazz, keyClazz, valueClazz);
+        Map <K,V> mapResult = mapper.readValue(jsonStr, javaType);
+        return mapResult;
+    }
+
+    public static <K, V> Map<K, V> readString(String jsonStr, Class <? extends Map> mapClazz ,JavaType keyJavaType,
+                                              JavaType valueJavaType) throws JsonParseException, JsonMappingException, IOException{
+        JavaType javaType = mapToJavaType(mapClazz, keyJavaType, valueJavaType);
+        Map <K,V> mapResult = mapper.readValue(jsonStr, javaType);
+        return mapResult;
+    }
+
+
     public static <T> T readJsonObject(JSONObject jsonObject, Class<T> clazz) throws IOException {
         return mapper.readValue(String.valueOf(jsonObject), clazz);
     }
 
-    public static <T> List<T> readCollection(String jsonStr, Class <? extends List> collectionClazz,
-              Class <T> elementClazz) throws IOException {
-        JavaType javaType = mapper.getTypeFactory().constructParametricType(collectionClazz, elementClazz);
-        List <T> listResult  = mapper.readValue(jsonStr, javaType);
-        return listResult;
+    /*以下是JavaType的转换方法*/
+    /**
+     * 简单类转换为JavaType对象
+     */
+    public static JavaType simpleClassToJavaType(Class <?> element){
+        JavaType javaType = typeFactory.uncheckedSimpleType(element);
+        return javaType;
     }
 
-    public static <K, V> Map<K, V> readMap(String jsonStr, Class <? extends Map> mapClazz ,Class<K> keyClazz,
-              Class<V> valueClazz) throws IOException {
-        JavaType javaType = mapper.getTypeFactory().constructMapType(mapClazz, keyClazz, valueClazz);
-        Map <K,V> mapResult = mapper.readValue(jsonStr, javaType);
-        return mapResult;
+    /**
+     * 简单的集合类型（即List<T>中的泛型为简单类）转换为JavaType
+     */
+    public static JavaType collectionToJavaType(Class <? extends List> collectionClazz, Class <?> elementClazz){
+        JavaType javaType = typeFactory.constructParametricType(collectionClazz, elementClazz);
+        return javaType;
+    }
+    /**
+     * 复合的集合类型转换为JavaType的方法，比如List<T>中的泛型为List<T>,此处采用collectionToJavaType的2个方法进行递归解析
+     * 递归思路为：将最里层的List<T>先转换为JavaType，则往外层得到List<JavaType>,接着继续解析得到该层的JavaType，重复操作，
+     * 直到最外层，得到最终能用于解析的JavaType对象
+     */
+    public static JavaType collectionToJavaType(Class <? extends List> collectionClazz, JavaType elementJavaType){
+        JavaType javaType = typeFactory.constructParametricType(collectionClazz, elementJavaType);
+        return javaType;
+    }
+
+    /**
+     * 用于将简单的Map<K,V>转换为JavaType对象
+     * */
+    public static JavaType mapToJavaType(Class <? extends Map> mapClazz ,Class<?> keyClazz,
+             Class<?> valueClazz){
+        JavaType javaType = typeFactory.constructMapType(mapClazz, keyClazz, valueClazz);
+        return javaType;
+    }
+    /**
+     * 用于将复合的Map<K,V>转换为JavaType对象
+     * */
+    public static JavaType mapToJavaType(Class <? extends Map> mapClazz, JavaType keyJavaType,
+            JavaType valueJavaType){
+        JavaType javaType = typeFactory.constructMapType(mapClazz, keyJavaType, valueJavaType);
+        return javaType;
     }
 }
